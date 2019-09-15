621838ff4211d24f5eaf58d6394a64429c1c3320
Edgar <ets@pm.me>
2019-09-15 13:54:53 +0300

    martic

--- lib/fraud_network.ex
+++ lib/fraud_network.ex
@@ -3,16 +3,26 @@ defmodule FraudNetwork do
   Documentation for FraudNetwork.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FraudNetwork.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def get_fraud_score(user, links, fraudulent_users, depth) do
+      matrix = Matrix.create_matrix_from_links(links, %{})
+      step(user, matrix, fraudulent_users, [], 0)
   end
+
+  def step(current_user, matrix, fraudulent_users, visited, score) do
+    fraud_count = get_fraudulent_count(matrix[current_user], fraudulent_users, visited, 0)
+    
+  end
+
+  def get_fraudulent_count([user|tail], fraudulent_users, visited, count) do
+    if Enum.member?(fraudulent_users, user) && !Enum.member?(visited, user) do
+      get_fraudulent_count(tail, fraudulent_users, visited, count + 1)
+    else 
+      get_fraudulent_count(tail, fraudulent_users, visited, count)
+    end
+  end
+
+  def get_fraudulent_count([], _, _, count) do
+    count
+  end
+
 end
