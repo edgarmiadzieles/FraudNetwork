7e642f40251117ab51be609c76b70dda9becd759
Edgar <ets@pm.me>
2019-09-15 14:19:20 +0300

    calculate fraudulency score for one level

--- lib/fraud_network.ex
+++ lib/fraud_network.ex
@@ -5,24 +5,37 @@ defmodule FraudNetwork do
 
   def get_fraud_score(user, links, fraudulent_users, depth) do
       matrix = Matrix.create_matrix_from_links(links, %{})
-      step(user, matrix, fraudulent_users, [], 0)
+      get_depth_score(user, matrix, fraudulent_users, [], depth, 0)
   end
 
-  def step(current_user, matrix, fraudulent_users, visited, score) do
-    fraud_count = get_fraudulent_count(matrix[current_user], fraudulent_users, visited, 0)
-    
+  def get_depth_score(current_user, matrix, fraudulent_users, visited, depth, score) do
+    {visited_all, found_links} = get_unvisited_links(matrix[current_user], visited, [])
+    fraudulent_count = get_fraudulent_count(found_links, fraudulent_users, 0)
+    (Enum.count(found_links) * fraudulent_count) / depth
   end
 
-  def get_fraudulent_count([user|tail], fraudulent_users, visited, count) do
-    if Enum.member?(fraudulent_users, user) && !Enum.member?(visited, user) do
-      get_fraudulent_count(tail, fraudulent_users, visited, count + 1)
+  def get_unvisited_links([user|tail], visited, found_links) do
+    if Enum.member?(visited, user) do
+      get_unvisited_links(tail, [user] ++ visited, found_links)
     else 
-      get_fraudulent_count(tail, fraudulent_users, visited, count)
+      get_unvisited_links(tail, [user] ++ visited, [user] ++ found_links)
     end
   end
 
-  def get_fraudulent_count([], _, _, count) do
+  def get_unvisited_links([], visited, found_links) do
+    { visited, found_links }
+  end
+
+  def get_fraudulent_count([], _, count) do
     count
+  end
+
+  def get_fraudulent_count([user|tail], fraudulent_users, count) do
+    if Enum.member?(fraudulent_users, user) do
+      get_fraudulent_count(tail, fraudulent_users, count + 1)
+    else
+      get_fraudulent_count(tail, fraudulent_users, count)
+    end
   end
 
 end
